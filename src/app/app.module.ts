dd7d8b62e48a11ad6cb0a941bd730518ac4d17ef
ChrisLaurian <laurian.reyes@gmail.com>
2020-03-02 22:12:43 -0600

    Cabecera Lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavComponent } from './nav/nav.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavComponent ],
+  declarations: [ AppComponent, HelloComponent, NavComponent, CabeceraComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
